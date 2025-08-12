778bbaa39582800b49b043b5efd134039fd757e3
PaulMue0 <muellerpaul2000@live.de>
2025-08-12 17:39:51 +0200

    current nvim setup

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,3 +8,19 @@ vim.keymap.set("n", "-", ":", { noremap = true })
 vim.keymap.set("n", "<leader>gg", function()
   require("neogit").open()
 end, { desc = "Open Neogit" })
+
+-- Unmap the default Ctrl+a (optional)
+vim.api.nvim_set_keymap("n", "<C-a>", "", { noremap = true, silent = true })
+
+-- Map Ctrl+y to increment number (same as Ctrl+a)
+vim.api.nvim_set_keymap("n", "<C-y>", "<C-a>", { noremap = true, silent = true })
+
+-- Delete blink.cmp accept feature
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = function()
+    vim.schedule(function()
+      pcall(vim.api.nvim_buf_del_keymap, 0, "i", "<C-y>")
+      vim.api.nvim_buf_set_keymap(0, "i", "<C-y>", "<C-y>", { noremap = true })
+    end)
+  end,
+})
