515c4f83150425a97e31917689bfff76438b4693
Paulmue0 <muellerpaul2000@live.de>
2026-02-04 15:53:35 +0100

    nvim: cleaner indexing

--- nvim/lua/plugins/harpoon2.lua
+++ nvim/lua/plugins/harpoon2.lua
@@ -1,26 +1,42 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local harpoon = require("harpoon")
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+    },
+  },
+  keys = function()
+    local keys = {
+      {
+        "<leader>H",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "Harpoon File",
+      },
+      {
+        "<leader>h",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Harpoon Quick Menu",
+      },
+    }
 
-    -- REQUIRED
-    harpoon:setup()
-
-    vim.keymap.set("n", "<leader>H", function()
-      harpoon:list():add()
-    end)
-    vim.keymap.set("n", "<leader>h", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
-
-    -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<C-S-P>", function()
-      harpoon:list():prev()
-    end)
-    vim.keymap.set("n", "<C-S-N>", function()
-      harpoon:list():next()
-    end)
+    for i = 1, 9 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          require("harpoon"):list():select(i)
+        end,
+        desc = "Harpoon to File " .. i,
+      })
+    end
+    return keys
   end,
 }
