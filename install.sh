4312ee7e3cb9229be842e973391d5c094bd33cc6
Paulmue0 <muellerpaul2000@live.de>
2026-02-04 12:13:10 +0100

    setup: install.sh script

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ brew install stow
 
 #--- stow dotfiles
 #--------- MAKE SURE ALL TARGET PATH EXIST
-stow nvim
-stow tmux
 stow zshrc
+stow tmux
+stow --target="$HOME/.config/nvim/" nvim
 stow --target="$HOME/.config/karabiner/" karabiner
 stow --target="$HOME/Library/Application Support/com.mitchellh.ghostty" ghostty
 stow --target="$HOME/.config/git/" git
